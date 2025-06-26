3e96943c96b82fa5ed0cd75e4a37ba077d7ba8a2
r05i36 <r05i36@ed.cc.suzuka-ct.ac.jp>
2025-06-26 10:12:58 +0900

    feat: 10-2 finish

--- zenki/10-2.cpp
+++ zenki/10-2.cpp
@@ -1,18 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-int partition(int a[], int p, int r);
-void quicksort(int a[], int p, int r);
+struct Card {
+    int num = 0;
+    char mark;
+};
+int partition(Card a[], int p, int r);
+void quicksort(Card a[], int p, int r);
+bool compare_by_num(const Card& a, const Card& b) {
+    return a.num < b.num;
+}
 
 int main() {
-    int n, a[100000];
+    int n;
     cin >> n;
+    vector<Card> b(n);
+    Card a[n];
 
     for(int i=0; i<n; i++) {
-        
+        cin >> a[i].mark >> a[i].num;
+        b[i].mark = a[i].mark;
+        b[i].num = a[i].num;
+    }
+    quicksort(a, 0, n-1);
+    stable_sort(b.begin(), b.end(), compare_by_num);
+
+    int count = 0;
+    for (int i=0; i<n; i++){
+        if(a[i].mark != b[i].mark) {
+            count++;
+        }
+    }
+    if(count > 0) cout << "Not stable" << endl;
+    else cout << "Stable" << endl;
+    
+    for(const Card& A : a) {
+        if(A.num != 0) cout << A.mark << " " << A.num << endl;
     }
 }
 
-void quicksort(int a[], int p, int r) {
+void quicksort(Card a[], int p, int r) {
     if(p < r) {
         int q = partition(a, p, r);
         quicksort(a, p, q-1);
@@ -20,11 +46,11 @@ void quicksort(int a[], int p, int r) {
     }
 }
 
-int partition(int a[], int p, int r) {
-    int x = a[r];
+int partition(Card a[], int p, int r) {
+    int x = a[r].num;
     int i = p-1;
     for(int j=p; j<r; j++) {
-        if(a[j] <= x) {
+        if(a[j].num <= x) {
             i++;
             swap(a[i], a[j]);
         }
